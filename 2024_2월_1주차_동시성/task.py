3abf887561806e0b5e1e16e6a4d353ce8837f2ff
Moon Yong Joon <myjlms99@gmail.com>
2024-01-23 23:06:58 +0900

    동시성 처리

---
+++ 2024_2월_1주차_동시성/task.py
@@ -0,0 +1,11 @@
+import time
+
+def task1():
+    for _ in range(5):
+        print("Task 1 is running")
+        time.sleep(1)
+
+def task2():
+    for _ in range(5):
+        print("Task 2 is running")
+        time.sleep(1)
